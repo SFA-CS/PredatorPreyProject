6ca8c8590f4098d0e292653e18b119a171727419
EldredKirin <swopeb@jacks.sfasu.edu>
2024-03-04 16:47:43 -0600

    Added Comments

--- Prey-Predator-Honors-Project/Assets/Script/MoveToMouse.cs
+++ Prey-Predator-Honors-Project/Assets/Script/MoveToMouse.cs
@@ -22,8 +22,8 @@ public class MoveToMouse : MonoBehaviour
     {
         moveableObjects.Add(this);
         target = transform.position;
-        player = this.prey;
-        player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        player = this.prey; //sets the first player to be prey
+        player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue; //sets the player to blue
 
     }
 
@@ -36,60 +36,44 @@ public class MoveToMouse : MonoBehaviour
             counter++;
             if (counter == 5)
             {
-                player.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                player.gameObject.GetComponent<SpriteRenderer>().color = Color.white; //sets the player to white
                 player = this.predator; //changes camera to the predator
-                player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue; //sets the new player to blue
             }
             if (counter == 10)
             {
-                player.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                player.gameObject.GetComponent<SpriteRenderer>().color = Color.white; //sets the player to white
                 player = this.prey; //changes camera to the prey
-                player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+                player.gameObject.GetComponent<SpriteRenderer>().color = Color.blue; //sets the new player to blue
                 counter = 0;
             }
 
-            //look to target
-            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 dir = Input.mousePosition - pos;
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-            float dist = dir.magnitude;
-
-                //move to target if its in range
-            if (dist <= 30) // setting the range
+            if(player.gameObject.GetComponent<SpriteRenderer>().color == Color.blue)
             {
-                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                target.z = transform.position.z;    
+                //look to target
+                Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+                Vector3 dir = Input.mousePosition - pos;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+                float dist = dir.magnitude;
+
+                if (dist <= 30) // setting the range
+                {
+                    //Have to be in the range to move to the clicked point
+                    target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    target.z = transform.position.z;
+                }
             }
+
+                
         }
+        //move to target if its in range
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
     }
-    
-    //if (Input.GetMouseButtonDown(0) && selected)
-    //{
-
-    //    target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //    target.z = transform.position.z;
-    //}
-
-    // transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
-    // }
-
 
     private void OnMouseDown()
     {
-        //Sets the color of the player to blue if selected and white if not
-        //selected = true;
-        //gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
 
-        //foreach(MoveToMouse obj in moveableObjects)
-        //{
-            //if(obj != this)
-            //{
-                //obj.selected = false;
-                //obj.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
-            //}
-        //}
     }
 
 
