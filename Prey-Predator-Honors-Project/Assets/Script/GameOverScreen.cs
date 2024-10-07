b33b9c1c3ca6f9552bdf07130569790e6e14397f
Boezienko <boeazienko@gmail.com>
2024-10-07 16:57:47 -0500

    Made reseting custom scene get closer to working
    Made it so the predator and preys go back to their starting location.
    - still need to turn off game over scene and reset numberPreyRemaining and numberOfTerms and Zoom

--- Prey-Predator-Honors-Project/Assets/Script/GameOverScreen.cs
+++ Prey-Predator-Honors-Project/Assets/Script/GameOverScreen.cs
@@ -10,6 +10,11 @@ public class GameOverScreen : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI WinnerText;
     public static GameOverScreen Instance { get; private set; }
+    private SpriteLoader spriteLoader;
+
+    
+    private Vector3[] preyTransforms = { new Vector3(-5, 0, 0), new Vector3(-8, 2, 0), new Vector3(-8, -2, 0) };
+    private Vector3[] predatorTransforms = { new Vector3(5, 0, 0), new Vector3(8, 2, 0), new Vector3(8, -2, 0) };
 
     private void Awake()
     {
@@ -32,11 +37,60 @@ public class GameOverScreen : MonoBehaviour
 
     public void RestartButton()
     {
-        SceneManager.LoadScene("MissileShipGame"); //if click on restart button game is reset
+        //SceneManager.LoadScene("CustomGame"); //if click on restart button game is reset
+        movePredatorToStart();
+        movePreyToStart();
+
     }
 
     public void MenuButton()
     {
         SceneManager.LoadScene("MainMenu"); //if click on main menu button you are sent to the menu
+    }
+
+    private void movePredatorToStart()
+    {
+        GameObject PredatorParent = GameObject.Find("Predator");
+        Debug.Log("Here");
+        int numToActivate = PlayerPrefs.GetInt(GameOptions.PREDATOR_NUMBER);
+        int i = 0;
+        // Go through each child of the parent game object
+        foreach (Transform child in PredatorParent.transform)
+        {
+            child.gameObject.SetActive(true);
+            child.localPosition = predatorTransforms[i];
+            child.localRotation = Quaternion.Euler(0, 0, 90);
+            numToActivate--;
+
+            if(numToActivate <= 0)
+            {
+                break;
+            }
+        }
+    }
+
+    private void movePreyToStart()
+    {
+        GameObject PreyParent = GameObject.Find("Prey");
+        Debug.Log("Here1");
+        int numToActivate = PlayerPrefs.GetInt(GameOptions.PREY_NUMBER);
+        int i = 0;
+        Debug.Log("Here2");
+        // Go through each child of the parent game object
+        foreach (Transform child in PreyParent.transform)
+        {
+            child.gameObject.SetActive(true);
+            child.localPosition = preyTransforms[i];
+            child.localRotation = Quaternion.Euler(0, 0, -90);
+            numToActivate--;
+
+            if (numToActivate <= 0)
+            {
+                break;
+            }
+            i++;
+        }
+
+        
     }
 }
